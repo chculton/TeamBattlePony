e0488385f43d9673119399745ab4e09b99f04489
Csumbavamba <zsombor.pirok@gmail.com>
2018-04-17 16:39:34 +1200

    Created an Empty Colosseum Level
    I created an empty level called Colosseum which is under the _Levels folder.
    
    Please don't upload to master and wait with any commits or push until we fixed merge rights

---
+++ DrunkenKnightInHell/Source/DrunkenKnightInHell/DrunkenKnightInHellGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DrunkenKnightInHellGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DRUNKENKNIGHTINHELL_API ADrunkenKnightInHellGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
